425f2d4943e56a66f1d4beb0d1e9b0f9c1c807fc
Iker Ituarte <iker.ituarte.tejedor@gmail.com>
2026-06-23 18:58:05 +0200

    Fix v0.21.0e metadata encoding

--- src/config/appVersion.ts
+++ src/config/appVersion.ts
@@ -1,3 +1,3 @@
-﻿export const APP_VERSION = "v0.21.0e";
+export const APP_VERSION = "v0.21.0e";
 export const APP_VERSION_NAME = "Career effective rating balance";
 export const APP_STATUS = "Compresión de rating efectiva solo en modo carrera";
